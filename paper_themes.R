4b4bfdfc7c4cd28113022e40fc1715034764cd80
zjiang2 <zhuoxunj@gmail.com>
2019-10-30 13:19:11 -0500

    added paper theme

---
+++ paper_themes.R
@@ -0,0 +1,11 @@
+
+# source this file into your script and add the theme to figures
+
+library(tidyverse)
+
+paper_base_theme_ <- theme(panel.border = element_blank(),
+                panel.grid.major = element_blank(),
+                panel.grid.minor = element_blank(),
+                axis.line = element_line(colour = "#170a45", size = .5),
+                axis.ticks = element_line(colour = "#170a45", size = .2),
+                axis.text = element_text(color = '#170a45'))
