172fabb1bfdd28d22c42d3cc603ba7501f907d84
zjiang2 <zhuoxunj@gmail.com>
2019-11-04 15:38:49 -0600

    added white background

--- paper_themes.R
+++ paper_themes.R
@@ -6,6 +6,7 @@ library(tidyverse)
 paper_base_theme_ <- theme(panel.border = element_blank(),
                 panel.grid.major = element_blank(),
                 panel.grid.minor = element_blank(),
+		panel.background = element_rect(fill = "white"),
                 axis.line = element_line(colour = "#170a45", size = .5),
                 axis.ticks = element_line(colour = "#170a45", size = .2),
                 axis.text = element_text(color = '#170a45'))
