d5f7770047da4de3caeb80af3f83bfc45e7752a1
zjiang2 <zhuoxunj@gmail.com>
2019-11-05 12:57:48 -0600

    added size parameter

--- paper_themes.R
+++ paper_themes.R
@@ -3,10 +3,13 @@
 
 library(tidyverse)
 
-paper_base_theme_ <- theme(panel.border = element_blank(),
-                panel.grid.major = element_blank(),
-                panel.grid.minor = element_blank(),
-		panel.background = element_rect(fill = "white"),
-                axis.line = element_line(colour = "#170a45", size = .5),
-                axis.ticks = element_line(colour = "#170a45", size = .2),
-                axis.text = element_text(color = '#170a45'))
+paper_base_theme_ = function(base_size=15)
+{
+  theme_bw(base_size = base_size) + 
+  theme(panel.border = element_blank(),
+  panel.grid.major = element_blank(),
+  panel.grid.minor = element_blank(),
+  axis.line = element_line(colour = "#170a45", size = .5),
+  axis.ticks = element_line(colour = "#170a45", size = .2),
+  axis.text = element_text(color = '#170a45'))
+}
