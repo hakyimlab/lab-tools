b256f60cf2deea641f19505470a5d26d522a218a
zjiang2 <zhuoxunj@gmail.com>
2019-11-20 13:42:07 -0600

    added density base theme

--- paper_themes.R
+++ paper_themes.R
@@ -25,3 +25,16 @@ bar_base_theme_ = function(base_size=15)
   axis.text = element_text(color = '#170a45')),
   scale_y_continuous(expand=c(0,0)))
 }
+
+density_base_theme_ = function(base_size=15)
+{
+  list(theme_bw(base_size = base_size),
+  theme(panel.border = element_blank(),
+  panel.grid.major = element_blank(),
+  panel.grid.minor = element_blank(),
+  axis.line = element_line(colour = "#170a45", size = .5),
+  axis.ticks = element_line(colour = "#170a45", size = .2),
+  axis.text = element_text(color = '#170a45')),
+  scale_y_continuous(expand=c(0,0)),
+  scale_x_continuous(expand=c(0,0)))
+}
