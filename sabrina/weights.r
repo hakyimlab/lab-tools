0d1d45137bc1891805fcd9c6dac74b6740642825
Sabrina Mi <sabrinami@Sabrinas-MBP.attlocal.net>
2020-05-08 21:31:09 -0400

    code for weights table

--- sabrina/weights.r
+++ sabrina/weights.r
@@ -3,22 +3,36 @@ library(tidyverse)
 
 setwd("~/Desktop/PEC_TWAS_weights")
 
+
 # make dataframe 
 # load("ENSG00000000457.wgt.RDat")
 make_df <- function(file) {
   load(file)
   weights <- data.frame(wgt.matrix)
-  for(i in 1:nrow(weights)) {
-    gene <- c(substr(file, 1, nchar(file) - 9))
-  }
-  weights$gene <- gene
-  weights$variant <- rownames(weights)
+  snps <- data.frame(snps)
   rownames(weights) <- c()
-  weights %>% select(gene, variant, top1, blup, bslmm)
+  weights$gene <- substr(file, 1, nchar(file) - 9)
+  weights$variant <- paste(snps$V2, snps$V5, snps$V6, sep=":")
+  weights %>% filter(enet != 0) %>% select(gene, variant, enet)
 }
 
 # append files
-
 files <- list.files(pattern = "\\.RDat")
 
+# connect to database
+conn <- dbConnect(RSQLite::SQLite(), "PEC_TWAS_weights.db")
+for(i in 1:length(files)) {
+  dbWriteTable(conn, "weights", make_df(files[i]), append = TRUE)
+}
 
+dbWriteTable(conn, "snps_matched", read.delim("snps_matched.txt"))
+
+weights_df <- dbGetQuery(conn, 'SELECT gene, rsid, panel_variant_id, non_effect_allele, effect_allele, enet FROM weights INNER JOIN snps_matched on weights.variant = snps_matched.variant')
+weights_df <- rename(weights_df, weight = enet, varID = panel_variant_id)
+n.snps <- weights_df %>% group_by(gene) %>% summarise(n.snps.in.model = n())
+# weights_df <- dbGetQuery(conn, 'SELECT gene, rsid, varID, non_effect_allele, effect_allele, weight FROM weights')
+
+dbWriteTable(conn, "weights", weights_df, overwrite= TRUE)
+dbRemoveTable(conn, "snps_matched")
+# disconnect
+dbDisconnect(conn)
